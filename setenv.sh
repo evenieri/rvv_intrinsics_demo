4bd63a4d1e9fc090e9bd8364ad62183e0d35b8fa
evenieri <emanuelevenieri@gmail.com>
2024-12-18 08:55:54 -0500

    added template for exercise and setenv

---
+++ setenv.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#
+export LD_LIBRARY_PATH=/opt/share/milkv/gcc-14.2.0/gcc-build-14.2.0/lib:/lib64:$LD_LIBRARY_PATH
+export PATH=/opt/share/milkv/gcc-14.2.0/gcc-build-14.2.0/bin:/opt/share/milkv/gcc-14.2.0/binutils-build-2.42/bin:$PATH
+export LIBRARY_PATH=/opt/share/milkv/gcc-14.2.0/gcc-build-14.2.0/lib:$LIBRARY_PATH

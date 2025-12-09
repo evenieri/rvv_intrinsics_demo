13af2f50c45d62ab88b830f6c344f5338a552569
evenieri <emanuelevenieri@gmail.com>
2025-12-09 19:20:46 +0800

    vector-avg to rvv 1.0, deleted simple vector-add

--- vector-add.c
+++ vector-add.c
@@ -4,12 +4,13 @@
 #include <stdlib.h>
 
 #include <riscv_vector.h>
-#define N 16
+#define N 2048
 
 
 void fill_random(uint8_t *vec, size_t length, int min, int max);
 void golden_sum(uint8_t * a, uint8_t * b, uint8_t * c);
 void intrinsics_sum(uint8_t * a, uint8_t *b, uint8_t * c);
+void result_cmp(uint8_t *a, uint8_t *b, size_t length);
 uint64_t start, end, cycles_golden, cycles_intrinsics;
 
 int main(){
@@ -20,16 +21,14 @@ int main(){
 	uint8_t Z2[N] = {0};
 
 	srand((unsigned int)time(NULL));
-	fill_random(X,N,0,127);
-	fill_random(Y,N,0,127);
+	fill_random(X, N, 0, 127);
+	fill_random(Y, N, 0, 127);
 
-	golden_sum(X,Y,Z1);
+	golden_sum(X, Y, Z1);
 
-	intrinsics_sum(X,Y,Z2);
+	intrinsics_sum(X, Y, Z2);
 
-	for(int i = 0; i < N; i++){
-	printf("%u %u\n", Z1[i], Z2[i]);
-	}
+	result_cmp(Z1, Z2, N);
 
 	printf("Golden_seconds: %f\nIntrinsics_seconds: %f\n", (float)cycles_golden/CLOCKS_PER_SEC, (float)cycles_intrinsics/CLOCKS_PER_SEC);
 
@@ -38,31 +37,33 @@ return 0;
 
 // C coded vector sum with simple for loop
 void golden_sum(uint8_t * a, uint8_t * b, uint8_t * c){
-	//asm volatile ("rdcycle %0" : "=r" (start));
 	start = clock();
 		for (int i = 0; i < N; i++ ){
 			c[i] = a[i] + b[i]; //dumb vector sum
 		}
 
-	//asm volatile ("rdcycle %0" : "=r" (end));
 	end = clock();
 	cycles_golden = end-start;
 }
 
 // intrinsics coded vector sum with the vector contained in a single vector register
 void intrinsics_sum(uint8_t * a, uint8_t *b, uint8_t * c){
-	//asm volatile ("rdcycle %0" : "=r" (start));	
 	start = clock();
-		size_t vlmax = __riscv_vsetvlmax_e8m1();		//set of vlmax given SEW=8 and LMUL=1
+	
+	size_t i=0;
+	size_t vlmax = __riscv_vsetvl_e8m1(N-i);
+	
+	for(i; i < N ; i += vlmax){
+		vlmax = __riscv_vsetvl_e8m1(N-i);		//set of vlmax given SEW=8 and LMUL=1
 									//
-		vuint8m1_t v0 = __riscv_vle8_v_u8m1 (a, vlmax);	//vector load for byte-unsigned, LMUL=1
-		vuint8m1_t v1 = __riscv_vle8_v_u8m1 (b, vlmax);
+		vuint8m1_t v0 = __riscv_vle8_v_u8m1 (&a[i], vlmax);		//vector load for byte-unsigned, LMUL=1
+		vuint8m1_t v1 = __riscv_vle8_v_u8m1 (&b[i], vlmax);
 
-		v0 = __riscv_vadd_vv_u8m1 (v0, v1, vlmax);	//vector add for bytes, LMUL=1
+		v0 = __riscv_vadd_vv_u8m1 (v0, v1, vlmax);		//vector add for bytes, LMUL=1
 
-		__riscv_vse8_v_u8m1 (c, v0, vlmax);			//vector store for byte-unsigned, LMUL=1
+		__riscv_vse8_v_u8m1 (&c[i], v0, vlmax);			//vector store for byte-unsigned, LMUL=1
+	}
 
-	//asm volatile ("rdcycle %0" : "=r" (end));
 	end = clock();
 	cycles_intrinsics = end-start;
 }
@@ -70,7 +71,19 @@ void intrinsics_sum(uint8_t * a, uint8_t *b, uint8_t * c){
 
 void fill_random(uint8_t *vec, size_t length, int min, int max) {
 
-    for (int i = 0; i < length; i++) {
+    for (size_t i = 0; i < length; i++) {
         vec[i] = min + rand() % (max - min + 1);
     }
+}
+
+void result_cmp(uint8_t *a, uint8_t *b, size_t length){
+	
+	size_t check = 0;
+	for(size_t i = 0; i < length; i++){
+		check += a[i] - b[i];
+	}
+	if (check){
+		printf("Error\n");
+	} else printf("Good!\n");
+
 }
--- vector-avg.c
+++ vector-avg.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <time.h>
 
-#include <riscv_th_vector.h>
-#define N 8192
+#include <riscv_vector.h>
+#define N 16384
 #define M 128
 
 void golden_avg(uint8_t * a, uint32_t * c);
@@ -16,7 +17,7 @@ int main(){
 	
 
 	uint8_t X[N];
-	srand(0xdeafbeef);
+	srand((unsigned int)time(NULL));
 	for(int i = 0; i < N; i++){
 		X[i] = rand() % M;
 	} //random vector 
@@ -30,15 +31,14 @@ int main(){
 
 	check_avg(Z1, Z2);
 
-	printf("Golden_cycles: %llu\nIntrinsics_cycles: %llu\n", cycles_golden, cycles_intrinsics);
+	printf("Golden_seconds: %f\nIntrinsics_seconds: %f\n", (float)cycles_golden/CLOCKS_PER_SEC, (float)cycles_intrinsics/CLOCKS_PER_SEC);
 
 return 0;
 }
 
 
 void golden_avg(uint8_t * a, uint32_t * c){
-	asm volatile ("rdcycle %0" : "=r" (start));
-
+	start = clock();
 	uint32_t acc = 0;
 
 		for (int i = 0; i < N; i++ ){
@@ -48,17 +48,17 @@ void golden_avg(uint8_t * a, uint32_t * c){
 		acc = acc/N;
 	*c = acc;
 
-	asm volatile ("rdcycle %0" : "=r" (end));
+	end = clock();
 	cycles_golden = end-start; 
 }
 
 void intrinsics_avg(uint8_t * a, uint32_t * c){
-	asm volatile ("rdcycle %0" : "=r" (start)); //do not touch, performance monitoring
+	start = clock(); //do not touch, performance monitoring
 
-	vuint32m1_t v_sum;
 	uint32_t sum;
-	vuint8m4_t v0;
 	size_t vlmax;
+	vuint32m1_t v_sum;
+	vuint8m4_t v0;
 	vuint16m8_t v_acc, v_zero_16;
 	vuint32m1_t v_zero;
 
@@ -66,21 +66,22 @@ void intrinsics_avg(uint8_t * a, uint32_t * c){
 	v_acc = __riscv_vmv_v_x_u16m8(0, vlmax); 	//initialize accumulator
 	v_zero = __riscv_vmv_v_x_u32m1(0, 4);		//initialize variable needed by wredsumu
 
-		for (int i = 0; i < N; i += vlmax ){
-		vlmax = __riscv_vsetvl_e16m8(N-i);						//stripmining
+	for (int i = 0; i < N; i += vlmax ){
+		vlmax = __riscv_vsetvl_e16m8(N-i);					//stripmining
 
-		v0 = __riscv_th_vlbu_v_u8m4_tu (v0, &a[i], vlmax);				//vector load for byte-unsigned, LMUL=4
-		v_acc = __riscv_vwaddu_wv_u16m8_tu (v_acc, v_acc, v0, vlmax);			//vector widening add for bytes+LMUL=4 to half+LMUL=8
+		v0 = __riscv_vle8_v_u8m4_tu (v0, &a[i], vlmax);				//vector load for byte-unsigned, LMUL=4
+		v_acc = __riscv_vwaddu_wv_u16m8_tu (v_acc, v_acc, v0, vlmax);		//vector widening add for bytes+LMUL=4 to half+LMUL=8
 		}
 		
-		vlmax = __riscv_vsetvlmax_e16m8();
+	vlmax = __riscv_vsetvlmax_e16m8();
 
-	      	v_sum = __riscv_vwredsumu_vs_u16m8_u32m1 (v_acc, v_zero, vlmax);		// vector widening reduction sum, sums elemnts of a vector and 
-												// puts them in the first element of a wider-element vector
-		sum = __riscv_vmv_x_s_u32m1_u32(v_sum);						// moving first elemnt of the vector into a scalar variable
-		*c = sum/N;									// scalar division to extract avg value
+      	v_sum = __riscv_vwredsumu_vs_u16m8_u32m1 (v_acc, v_zero, vlmax);	// vector widening reduction sum, sums elemnts of a vector and 
+										// puts them in the first element of a wider-element vector
+									 
+	sum = __riscv_vmv_x_s_u32m1_u32(v_sum);					// moving first elemnt of the vector into a scalar variable
+	*c = sum/N;								// scalar division to extract avg value
 
-	asm volatile ("rdcycle %0" : "=r" (end)); //do not touch, performance monitoring
+	end = clock(); //do not touch, performance monitoring
 	cycles_intrinsics = end-start; //do not touch
 }
 
@@ -88,7 +89,7 @@ void check_avg (uint32_t a, uint32_t b){
 	int check = 0;
 	check = a-b;
 
-	if(!check) printf("OK!\n");
-       		else printf("Not OK :/\n");
+	if(check) printf("Error :/\n");
+       		else printf("Good!\n");
 
 }
